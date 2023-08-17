4cce5d85a7f3ccafc1a27b3204fe927385f59f7f
Shubham Patil <shubham.p@ibirdsservices.com>
2023-08-17 19:03:17 +0530

    author fix

--- routes/blogs.js
+++ routes/blogs.js
@@ -13,16 +13,13 @@ router.post("/newblog/:author",async (req, res)=>{
     
     const users = await runQuery.query(`SELECT id FROM express.users WHERE firstname = $1`,[req.params.author])
 
-    const authorOne = users.rows[1].id;
+    const author = users.rows[1].id;
 
-    const author = "a1790342-6502-418d-af48-baa8decb93f2";
-
-    console.log("authorOne --> ",users.rows[1].id)
-    console.log("author --> ",author)
+    console.log("author --> ",users.rows[1].id)
 
     const { title, content } = req.body;
 
-    const insertRes = await runQuery.query(`INSERT INTO express.blogs (title, content, author) VALUES ($1, $2, $3) RETURNING *;`,[title, content, authorOne])
+    const insertRes = await runQuery.query(`INSERT INTO express.blogs (title, content, author) VALUES ($1, $2, $3) RETURNING *;`,[title, content, author])
 
     res.json(insertRes.rows[0]);
 })
